46d07ed3284787976ee669eed9fb52d564512018
vishkashpvp <vishkash.vishnu@gmail.com>
2023-07-14 17:16:49 +0530

    make sidenav collapsible

--- client/src/app/components/layout/layout.component.ts
+++ client/src/app/components/layout/layout.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  isMobileScreen: boolean = false;
+  isDrawerOpen: boolean = true;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any) {
+    this.isMobileScreen = event.target.innerWidth < 1280;
+  }
+
+  toggleDrawer() {
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
+}
